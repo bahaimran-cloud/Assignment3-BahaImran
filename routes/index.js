5fdbb84a82bd9349e23674651ecc9e7b0636b652
bahaimran-cloud <baha.imran@ontariotechu.net>
2025-11-18 16:43:12 -0500

    Added Home, contact and partials

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home Page' });
 });
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Assessment Tracker - Home Page' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Assessment Tracker - Contact Me' });
+});
+
+
 
 module.exports = router;
